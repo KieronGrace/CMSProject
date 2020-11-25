987afe1542c3a87d4d20968ae2b12f6144778c07
Kieron Roberson <kieronroberson@gmail.com>
2020-11-25 17:15:47 -0600

    11/25/2020 5:15

--- mission.php
+++ mission.php
@@ -8,10 +8,10 @@ include('templates/menu.php');
   <img class="secondaryFig" width="640" height="426" src="http://kieroncodes.com/cmsproject/pics/baby_feet.jpg" alt="Mother's hands holding baby's feet">
   <div id="home_message">
     <p>Our mission here at Cupcake Love is to be the hands and feet of Christ to our community in two ways: </p>
-        <ol>
-          <li>By providing a high quality product and great customer service.</li>
-          <li>By partnering with local city pregnancy center to support local women through unplanned pregnancies.</li>
-        </ol>
+      <ol>
+        <li>By providing a high quality product and great customer service.</li>
+        <li>By partnering with local city pregnancy center to support local women through unplanned pregnancies.</li>
+      </ol>
     <p>For more information on how to get involved with local city pregnancy center please visit the <a href="https://thepregnancycarecenter.org/support-the-center/">volunteer</a> page on their website.</p>
   </div>
 </div>
